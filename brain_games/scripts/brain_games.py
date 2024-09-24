b85d21a040be0f21b4cec94d8594e8dde14053d8
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-09-24 11:04:10 +0300

    fix

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-import brain_games.cli as cli
+from brain_games.cli import welcome_user
 
 
 def main():
     print('Welcome to the Brain Games!')
-    cli.welcome_user()
+    welcome_user()
 
 
 if __name__ == '__main__':
